ee6dd9659068c0ee2aea5f88a2a138528807a26a
margaiwangara <margaiwangara@gmail.com>
2022-09-28 19:40:00 +0300

    implemented test requirements and ran tests that passed

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,27 @@
 import { httpGet } from './mock-http-interface';
 
 // TODO define this type properly
-type TResult = any;
+type TResultKeys = 'Arnie Quote' | 'FAILURE';
+type TResult = {
+  [K in TResultKeys]?: string;
+};
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
   // TODO: Implement this function.
-  
-  return [];
+  const responses = await Promise.all(urls.map((url) => httpGet(url)));
+
+  const results = responses.map((response) => {
+    const body = JSON.parse(response.body);
+    if (response.status === 200) {
+      return {
+        'Arnie Quote': body.message,
+      };
+    }
+
+    return {
+      FAILURE: body.message,
+    };
+  });
+
+  return results;
 };
